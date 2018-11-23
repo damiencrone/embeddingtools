69c275ae8d38acbbc28bc61de97d6508ba74f63f
Damien Crone <damien.crone@gmail.com>
2018-11-23 20:06:20 +1100

    Updated demos

--- R/findTopItems.R
+++ R/findTopItems.R
@@ -1,16 +1,23 @@
 #' Function to locate the most (dis)similar items to a concept.
 #' 
 #' @param concept_similarity an item by concept matrix
-#' @param decreasing logical denoting whether to search for most (TRUE) vs.
+#' @param decreasing logical denoting whether to search for most (TRUE) vs. 
 #'   least (FALSE) similar items
-#' @param n integer denoting the number of most (dis)similar items to find for
+#' @param n integer denoting the number of most (dis)similar items to find for 
 #'   each concept
-#' @return a list of the most (dis)similar items for each concept, with one
+#' @param exclude a character vector or list of character vectors of terms to
+#'   exclude
+#' @return a list of the most (dis)similar items for each concept, with one 
 #'   element for each concept, containing a named vector of similarities
 #' @export
-findTopItems = function (concept_similarity, decreasing = TRUE, n = 10) {
+findTopItems = function (concept_similarity, decreasing = TRUE, n = 10, exclude = NULL) {
   
   top_list = list()
+  
+  if (!is.null(exclude)) {
+    exclude_ind = rownames(concept_similarity) %in% unlist(exclude)
+    concept_similarity = concept_similarity[!exclude_ind, , drop = FALSE]
+  }
   
   for (concept_name in colnames(concept_similarity)) {
     
